e4407d119d00cd011b38c457926c394401bf95d9
Namrata-Keskar <namrata.keskar30@gmail.com>
2024-01-20 20:05:49 -0800

    changed modal

--- src/popUps/afterEvent.js
+++ src/popUps/afterEvent.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import "./afterEvent.css"
 
 const afterEvent = ({ open, onClose }) => {
+
   if (!open) return null;
   return (
     <div onClick={onClose} className='overlay'>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Krub:ital@1&family=Montserrat&family=Nunito:wght@500&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Krub&family=Montserrat&family=Nunito:wght@500&display=swap" rel="stylesheet" />
+
       <div
         onClick={(e) => {
           e.stopPropagation();
@@ -13,19 +22,11 @@ const afterEvent = ({ open, onClose }) => {
       >
         <div className='modalRight'>
           <p className='closeBtn' onClick={onClose}>
-            X
+            <FontAwesomeIcon icon={faTimes} />
           </p>
+
           <div className='content'>
-            <p>Do you want to add another event?</p>
-          </div>
-          <div className='btnContainer'>
-            <button className='btnPrimary'>
-              <span className='bold'>YES</span>
-            </button>
-            <button className='btnOutline'>
-              <span className='bold'>Go back to home page</span>
-              
-            </button>
+            <p>Event Added!</p>
           </div>
         </div>
       </div>

331e99d1cacfb19e681ad0f2f61191d35bd5cf5f
anusha <anu.rapuru@gmail.com>
2024-01-20 16:19:43 -0800

    modal pops up when submitting event and category is dynamically added to dropdown select menu, and saved into event data

--- src/pages/AddEvent.js
+++ src/pages/AddEvent.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import AfterEvent from '../popUps/afterEvent.js'
+
 import 'firebase/database'
 
 import "./AddEvent.scss";
@@ -14,14 +16,54 @@ function AddEvent() {
   const [eventDate, setEventDate] = useState('');
   const [eventLocation, setEventLocation] = useState('');
   const [eventDescription, setEventDescription] = useState('');
+  const [openModal, setOpenModal] = useState(true);
+  const [selectedTopic, setSelectedTopic] = useState('');
+  const [topics, setTopics] = useState([]); // State to store the list of topics
+
+
+  // const collectionRef = firestore.collection(db, 'categories');
+  // const q = query(collectionRef, where("__id", "==", "food"));
+
+  
+  // useEffect(() => {
+  //   const catCollection = firestore.collection(db, 'categories');
+  //   console.log("fhdjshf");
+  //   catCollection.get().then((firestore.QuerySnapshot) => {
+  //     console.log('docs in collection:');
+  //     firestore.QuerySnapshot.forEach((doc) => {
+  //       console.log(doc.data());
+  //     });
+  //   });
+  //   // firestore.onSnapshot(firestore.collection(db, "categories"), (snapshot) => {
+  //   //   console.log("jii");
+  //   //   console.log(snapshot.docs.map(doc => doc.data()));
+  //   // });
+  // });
+
+  const getCollections = async () => {
+    try {
+      const dataCollection = firestore.collection(db, "topic");
+      const dataSnapshot = await firestore.getDocs(dataCollection);
+      console.log("data snapshot:", dataSnapshot);
+      const list = dataSnapshot.docs.map(doc => doc.data());
+      console.log("list:", list);
+      setTopics(list);
+
+    } catch {
+      console.log("error");
+    }
+  }
+  useEffect(() => {
+    getCollections();
+  }, []);
+
+  useEffect(() => {
+    // Log topics whenever it changes
+    console.log("topics in rendering:", topics);
+  }, [topics]);
+
 
   const handleSubmit = async () => { 
-    
-
-    // db.collection("event").doc().set({
-    //     name: eventLocation,
-    //     description: eventDescription,
-    // }).catch(alert);
     console.log("event name:", eventName);
     console.log("event date:", eventDate);
     console.log("event location:", eventLocation);
@@ -31,7 +73,7 @@ function AddEvent() {
     try {
       const docRef = await firestore.addDoc(eventsCollection, {
         name: eventName,
-        catId: "temp",
+        catId: selectedTopic,
         location: eventLocation,
         date: eventDate,
         description: eventDescription,
@@ -45,12 +87,41 @@ function AddEvent() {
     } catch (error) {
       console.error('Error adding document:', error);
     }
-
-  
   }
+  // Function that calls both the submit and push function of a button
+  const showModal = () =>{
+    setOpenModal(false)
+    handleSubmit()
+  };
+
+  // const useOptions = () => {
+  //   const [loading, setLoading] = useState(true);
+  //   const [posts, setPosts] = useState([]);
+  
+
+
+  // only add in an async function
+  // const snapShot = await getDoc(q);
+    // useEffect(() => {
+    //   const unsub = firestore.onSnapshot(collectionRef, (querySnapshot) => {
+    //     const items = [];
+    //     querySnapshot.forEach((doc) =>{
+    //       items.push(doc.data());
+    //       console.log("HII");
+    //       console.log(doc.data().__id);
+    //     });
+    //     return () => {
+    //       unsub();
+    //     }
+
+    //   })
+    // })
+
+ //}
 
   
   return (
+    // Chunk of code to add information about an event
     <div className="AddEvent">
         <p> ADD EVENT Page </p>
         <input
@@ -78,8 +149,44 @@ function AddEvent() {
           placeholder="Enter event description"
         ></textarea>
 
-        <button onClick={handleSubmit}>Submit</button>
-        {/* <button>Submit</button> */}
+      {/* Now add a drop down bar to see what category they will fall into */}
+      <select
+        value={selectedTopic}
+        onChange={(e) => setSelectedTopic(e.target.value)}
+      >
+        <option value={selectedTopic} disabled>Select a topic</option>
+        {topics.map((topic, index) => (
+          <option key={index} value={topic.id}>
+            {topic.id}
+          </option>
+        ))}
+      </select>
+
+
+      {/* <select value={selectedTopic} onChange={e=>setSelectedTopic(e.target.value)}>
+          {
+            topics.map(opt=><option>{opt}</option>)
+          }
+
+      </select> */}
+
+       
+      {/* Whem submit button clicked, information will be passed to firebase
+      and then the serivce provider will be asked if they want to add another event.
+      If not they will go back to the home page(probably get logged out) */}
+      <div>
+        <button 
+        onClick={() => showModal() } 
+        className='modalButton'>
+          afterEvent
+        </button>
+      <AfterEvent
+        open={openModal} 
+        onClose={() => setOpenModal(false)} />
+      </div>
+
+      
+
 
     </div>
   );

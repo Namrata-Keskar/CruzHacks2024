b356b8be365f75865927ff0e63eae56bfd99c96a
anusha <anu.rapuru@gmail.com>
2024-01-20 21:39:46 -0800

    created sahred component, with dynamic data from firebase, and updated food, housing, medical js

--- src/pages/Medical.js
+++ src/pages/Medical.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import app from '../firebase.js';
+import * as firestore from 'firebase/firestore';
+import SharedCard from '../card/SharedCard'; // Assuming SharedCard.js is in the same directory
+
+
+const db = firestore.getFirestore(app);
 
 function Medical() {
   return (
-    <div className="Medical">
-      
-        <p>
-            MEDICAL Page
-        </p>
-        
-    </div>
+    <SharedCard category="medical" />
   );
 }
 
